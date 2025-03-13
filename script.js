abd29340299a8f2af0e2ab3a6bc8fbefecf07b16
AndresRaphaelPFronteras <andresraphael.fronteras@smc.pshs.edu.ph>
2025-03-13 23:48:09 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+
+function runQuiz() {
+    prompt("1. Where do you live? <br>" +
+           "A. Luzon <br>" +
+           "B. Visayas <br>" +
+           "C. Mindanao <br>" +
+           "D. Abroad");
+    prompt("2. What is your monthly salary? <br>" +
+           "A. P 500 <br>" +
+           "B. P 1,000 <br>" +
+           "C. P 2,000 <br>" +
+           "D. P 5,000");
+    prompt("3. How is your family structured? <br>" +
+           "A. Only Child <br>" +
+           "B. One Sibling <br>" +
+           "C. Two Siblings <br>" +
+           "D. More Siblings");
+    prompt("4. Can you attend events? <br>" +
+           "A. Yes <br>" +
+           "B. No <br>" +
+           "C. Maybe <br>" +
+           "D. Maybe Not");
+    prompt("5. How are you doing mentally? <br>" +
+           "A. Fine <br>" +
+           "B. Not Fine <br>" +
+           "C. Working On It <br>" +
+           "D. Getting Help");
+    alert("We will send you an email in 2-3 days regarding how you can help!");
+}
+
